f0b1e6ff16cad708b3c8b5be9758034ccc88e9ad
muhdlaziem <muhdlaziem@gmail.com>
2022-03-07 00:45:24 +0800

    demo

---
+++ demo/go.mod
@@ -0,0 +1,3 @@
+module example.local/demo
+
+go 1.17
---
+++ demo/hello.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"math"
+
+	"github.com/muhdlaziem/go/repo"
+)
+
+func main() {
+	fmt.Println(math.Pi)
+	fmt.Println(repo.SayHi())
+}
